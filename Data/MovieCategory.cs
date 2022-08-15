7a875aaa3f972255574750ed11f2725af8c78636
csmohamme <cs.mohammedalmostafa@gmail.com>
2022-08-15 06:10:53 -0700

    create actor,cinema,movie and producer models

---
+++ Data/MovieCategory.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTicket.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
