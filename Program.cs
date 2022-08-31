6f4e59c7dcbce10040fc7683f48018eee21d8f25
Mohammed Almostafa <58958280+csmohamme@users.noreply.github.com>
2022-08-31 01:48:54 -0700

    edit the navbar

--- Program.cs
+++ Program.cs
@@ -26,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Movies}/{action=Index}/{id?}");
 AppDbInitializer.Seed(app);
 app.Run();
